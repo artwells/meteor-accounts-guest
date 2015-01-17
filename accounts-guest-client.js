d95d665a45297d22c05dc9dd248a9de8c9176b73
MichaelJenny <michael.jenny@projekt-und-partner.de>
2015-01-17 21:07:23 +0100

    Don't override Meteor.user() to deal with guest logins, as many packages want to deal with the user object, even if the logged in user is a guest user.
    The original intention for the override was to allow the
    accounts-base "Sign-In" to appear. This commit still allows exactly
    that, but does not return an empty user record when calling
    Meteor.user() as guest. (Which was not the expected behaviour anyway,
    I would argue. Because the method name implies that you get a user
    record, regardless if the user is guest or not.)

--- accounts-guest-client.js
+++ accounts-guest-client.js
@@ -4,22 +4,30 @@
 *
 */
 
-/* make anonymous users kinda non-users -- just ids
-* this allows the account-base "Sign-in" to still appear
-*/
-Meteor.user = function () {
-	var userId = Meteor.userId();
-	if (!userId) {
-		return null;
-	}
-	var user = Meteor.users.findOne(userId);
-	if (user !== undefined &&
-		user.profile !== undefined &&
-		user.profile.guest) {
-			return null;
-		}
-		return user;
-	};
+
+// If our app has a Blaze, override the {{currentUser}} helper to deal guest logins
+if (Package.blaze) {
+  /**
+   * @global
+   * @name  currentUser
+   * @isHelper true
+   * @summary Calls [Meteor.user()](#meteor_user). Use `{{#if currentUser}}` to check whether the user is logged in.
+   * Where "logged in" means: The user has has authenticated (e.g. through providing credentials)
+   */
+  Package.blaze.Blaze.Template.registerHelper('currentUser', function () {
+    var user = Meteor.user();
+    if (user &&
+        typeof user.profile !== 'undefined' &&
+        typeof user.profile.guest !== 'undefined' &&
+        user.profile.guest){
+        // a guest login is not a real login where the user is authenticated. 
+        // This allows the account-base "Sign-in" to still appear
+      return null;
+    }else{
+      return Meteor.user();
+    }
+  });
+}
 
 
 	//no non-logged in users
