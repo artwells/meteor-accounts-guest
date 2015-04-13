4e85a46b6d954c7a7fa8a330eb7c18236716a08c
artwells <artwells@gmail.com>
2015-04-12 17:20:30 -0700

    updating version for moniker addition

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Automatically add visitor as anonymous guest with userId",
-    version: "0.1.7",
+    version: "0.1.8",
     name: "artwells:accounts-guest",
     git: "https://github.com/artwells/meteor-accounts-guest.git"
 });
