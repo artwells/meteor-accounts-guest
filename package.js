12b8c22e0bb7f64613c0e4358485b9b60ead3d6e
artwells <artwells@gmail.com>
2015-05-14 11:48:44 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Automatically add visitor as anonymous guest with userId",
-    version: "0.1.10",
+    version: "0.1.11",
     name: "artwells:accounts-guest",
     git: "https://github.com/artwells/meteor-accounts-guest.git"
 });
