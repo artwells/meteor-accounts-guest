67ac77a998f4795737a4074a0b13298360a4120b
Stefan Baur <baursn@uni-math.gwdg.de>
2018-04-08 10:39:20 +0200

    update versions

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
     summary: "Automatically add visitor as anonymous guest with userId",
-    version: "0.1.16",
+    version: "0.1.17",
     name: "baursn:accounts-guest",
     git: "https://github.com/baursn/meteor-accounts-guest.git"
 });
 
 Package.onUse(function (api) {
     api.versionsFrom("METEOR@1.6.1");
-    api.use(['accounts-base','deps', 'blaze@2.0.4'], 'client');
-    api.use(['accounts-base', 'mongo@1.1.0', 'check', 'random'], 'server');
+    api.use(['accounts-base'], 'client');
+    api.use(['accounts-base', 'check', 'random'], 'server');
     api.use('accounts-password', 'server', { weak: true });
     api.use('underscore', 'server');
     api.use('brettle:accounts-patch-ui@0.0.1');
-    api.use('brettle:accounts-login-state@0.0.1');
+    api.use('brettle:accounts-login-state@0.0.4');
     api.use('brettle:accounts-add-service@0.3.0', ['client', 'server'], { weak: true });
     api.use('brettle:accounts-multiple@0.3.0', ['client', 'server'], { weak: true });
     api.add_files('accounts-guest.js', ['client','server']);
